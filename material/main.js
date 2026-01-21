4c0344bd6e8e7cc4215c25df1f49a459291eb11f
aiichirookuda <aiichirookuda@gmail.com>
2026-01-22 02:09:06 +0900

    マテリアルの種類について学習

---
+++ material/main.js
@@ -0,0 +1,87 @@
+import * as THREE from "./build/three.module.js";
+import { OrbitControls } from "./controls/OrbitControls.js";
+
+let scene, camera, renderer, pointLight, controls, sphere, plane, octahedron;
+
+window.addEventListener("load", init);
+
+function init() {
+  //シーン
+  scene = new THREE.Scene();
+
+  //カメラ
+  camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    100
+  );
+  camera.position.set(1, 1, 2);
+
+  //レンダラー
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  document.body.appendChild(renderer.domElement);
+
+  /**
+   * マテリアルセクション
+   */
+  // ジオメトリ
+  const sphereGeometry = new THREE.SphereGeometry(0.5, 16, 16);
+  const planeGeometry = new THREE.PlaneGeometry(1, 1);
+  const octahedronGeometry = new THREE.OctahedronGeometry(0.5);
+
+  // テクスチャ
+  const texture = new THREE.TextureLoader().load("./textures/brick.jpg");
+
+  // マテリアル
+  const material = new THREE.MeshBasicMaterial({ map: texture, color: 'red' });
+  material.side = THREE.DoubleSide;
+  material.opacity = 0.5;
+  material.transparent = true;
+
+
+  // メッシュ化
+  sphere = new THREE.Mesh(sphereGeometry, material);
+  plane = new THREE.Mesh(planeGeometry, material);
+  octahedron = new THREE.Mesh(octahedronGeometry, material);
+
+
+  sphere.position.x = -1.5;
+  octahedron.position.x = 1.5;
+
+  scene.add(sphere, plane, octahedron);
+
+  //マウス操作
+  const controls = new OrbitControls(camera, renderer.domElement);
+
+  window.addEventListener("resize", onWindowResize);
+
+  animate();
+}
+
+const clock = new THREE.Clock();
+
+function animate() {
+  const elapsedTime = clock.getElapsedTime();
+
+  // オブジェクトを回転させる
+  sphere.rotation.x = elapsedTime;
+  plane.rotation.x = elapsedTime;
+  octahedron.rotation.x = elapsedTime;
+  sphere.rotation.y = elapsedTime;
+  plane.rotation.y = elapsedTime;
+  octahedron.rotation.y = elapsedTime;
+
+  //レンダリング
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+}
+
+//ブラウザのリサイズに対応
+function onWindowResize() {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+}
